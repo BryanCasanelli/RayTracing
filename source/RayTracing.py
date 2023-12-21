177f8ed49d28a7f1708b6364d486e8e6c4b7bd1d
Bryan Casanelli <bcasanelli@dparadig.com>
2023-12-21 00:28:20 +0000

    Update RayTracing.py with Plotly graph and PyQt5 widgets.

--- source/RayTracing.py
+++ source/RayTracing.py
@@ -1,19 +1,55 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+import plotly.graph_objects as go
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Ray Tracing")
-        self.button = QPushButton("Press me!")
 
-        self.setCentralWidget(self.button)
+        # Create the "Open 3D file" button
+        self.button = QPushButton("Open 3D file")
+
+        # Create the space to display the Plotly graph
+        self.plot_widget = QWebEngineView()
+        self.plot_widget.setHtml('''
+            <html>
+            <head>
+                <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+            </head>
+            <body>
+                <div id="plot"></div>
+                <script>
+                    // Code to generate and display the Plotly graph
+                    var data = [{
+                        x: [1, 2, 3, 4, 5],
+                        y: [1, 4, 9, 16, 25],
+                        type: 'scatter'
+                    }];
+                    Plotly.newPlot('plot', data);
+                </script>
+            </body>
+            </html>
+        ''')
+
+        # Create the vertical layout and add the widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
+        layout.addWidget(self.plot_widget)
+
+        # Create a container widget and set the layout
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        # Set the container widget as the central widget
+        self.setCentralWidget(widget)
 
 def main():
     app = QApplication([])
     window = MainWindow()
-    window.show()
-    app.exec()
+    window.showMaximized()
+    app.exec_()
 
 if __name__ == "__main__":
     main()
