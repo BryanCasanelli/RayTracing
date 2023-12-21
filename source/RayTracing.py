d3fa5dc3355f3dd9711523b315124287bec1ee99
Bryan Casanelli <bcasanelli@dparadig.com>
2023-12-21 14:45:24 +0000

    Refactor RayTracing.py: Add file dialog for 3D file selection and update visualization.

--- source/RayTracing.py
+++ source/RayTracing.py
@@ -1,42 +1,32 @@
-import plotly.graph_objects as go
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog
+from Scene import Scene
+from Polyhedron import Polyhedron
+from vispy import scene
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        """
+        Initializes the main window of the Ray Tracing application.
+        """
         super().__init__()
 
+        # Set the window title
         self.setWindowTitle("Ray Tracing")
 
-        # Create the "Open 3D file" button
-        self.button = QPushButton("Open 3D file")
+        # Create a Scene instance
+        self.scene = Scene()
 
-        # Create the space to display the Plotly graph
-        self.plot_widget = QWebEngineView()
-        self.plot_widget.setHtml('''
-            <html>
-            <head>
-                <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-            </head>
-            <body>
-                <div id="plot"></div>
-                <script>
-                    // Code to generate and display the Plotly graph
-                    var data = [{
-                        x: [1, 2, 3, 4, 5],
-                        y: [1, 4, 9, 16, 25],
-                        type: 'scatter'
-                    }];
-                    Plotly.newPlot('plot', data);
-                </script>
-            </body>
-            </html>
-        ''')
+        # Create the "Open 3D file" button
+        self.button = QPushButton("Add 3D file")
+        self.button.clicked.connect(self.open_file_dialog)
+
+        # Create the VisPy canvas
+        self.vispy_canvas = scene.SceneCanvas(keys='interactive', bgcolor='white')
 
         # Create the vertical layout and add the widgets
         layout = QVBoxLayout()
         layout.addWidget(self.button)
-        layout.addWidget(self.plot_widget)
+        layout.addWidget(self.vispy_canvas.native)
 
         # Create a container widget and set the layout
         widget = QWidget()
@@ -45,7 +35,35 @@ class MainWindow(QMainWindow):
         # Set the container widget as the central widget
         self.setCentralWidget(widget)
 
+    def open_file_dialog(self):
+        """
+        Opens a file dialog to select an OBJ file.
+
+        Returns:
+            str: The selected file name.
+        """
+        file_name, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "resources/obj", "OBJ Files (*.obj)")
+        if file_name:
+            # Create a new Polyhedron from the OBJ file
+            polyhedron = Polyhedron(file_name)
+            
+            # Add the Polyhedron to the Scene
+            self.scene.add_object(polyhedron)
+
+            # Update the visualization
+            self.update_visualization()
+
+    def update_visualization(self):
+        """
+        Updates the visualization of the scene in the VisPy canvas.
+        """
+        self.scene.vispy_display(self.vispy_canvas)
+
 def main():
+    """
+    The main function of the RayTracing program.
+    Initializes the application, creates the main window, and starts the event loop.
+    """
     app = QApplication([])
     window = MainWindow()
     window.showMaximized()
