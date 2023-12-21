218384086f64612baeb982caef84423fe09694ba
Bryan Casanelli <bcasanelli@dparadig.com>
2023-12-21 22:20:57 +0000

    Refactor UI buttons and add move functionality.

--- source/RayTracing.py
+++ source/RayTracing.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QTableWidget, QTableWidgetItem, QHBoxLayout, QSplitter, QAbstractItemView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QTableWidget, QTableWidgetItem, QHBoxLayout, QSplitter, QAbstractItemView, QDialog, QDoubleSpinBox, QGridLayout, QLabel, QSizePolicy
 from Scene import Scene
 from Polyhedron import Polyhedron
 from vispy import scene
@@ -17,13 +17,16 @@ class MainWindow(QMainWindow):
         # Create a Scene instance
         self.scene = Scene()
 
-        # Create the "Add 3D object" button
-        self.add_button = QPushButton("Add 3D object")
+        # Create the "Add object" button
+        self.add_button = QPushButton("Add")
         self.add_button.clicked.connect(self.open_file_dialog)
 
         # Create the "Delete object" button
-        self.delete_button = QPushButton("Delete object")
+        self.delete_button = QPushButton("Delete")
         self.delete_button.clicked.connect(self.delete_selected_object)
+
+        self.move_button = QPushButton("Move")
+        self.move_button.clicked.connect(self.move_selected_object)
 
         # Create the VisPy canvas
         self.vispy_canvas = scene.SceneCanvas(keys='interactive', bgcolor='white')
@@ -43,6 +46,7 @@ class MainWindow(QMainWindow):
         self.buttons_layout.setSpacing(1)
         self.buttons_layout.addWidget(self.add_button)
         self.buttons_layout.addWidget(self.delete_button)
+        self.buttons_layout.addWidget(self.move_button)
         self.buttons_widget = QWidget()
         self.buttons_widget.setLayout(self.buttons_layout)
 
@@ -136,8 +140,117 @@ class MainWindow(QMainWindow):
         """
         if self.table_widget.selectedItems():
             self.delete_button.setEnabled(True)
+            self.move_button.setEnabled(True)
         else:
             self.delete_button.setEnabled(False)
+            self.move_button.setEnabled(False)
+
+    def move_selected_object(self):
+        """
+        Moves the currently selected objects based on user input.
+        """
+        dialog = MoveObjectDialog(self)
+        result = dialog.exec_()
+        if result == QDialog.Accepted:
+            dx, dy, dz = dialog.get_values()
+            selected_rows = sorted(set(index.row() for index in self.table_widget.selectedIndexes()))
+            for row in selected_rows:
+                # Move the object in the scene
+                self.scene.objects[row].translate(dx, dy, dz)
+
+        # Update the visualization and the table
+        self.update()
+
+class MoveObjectDialog(QDialog):
+    """
+    A dialog window for moving an object in a 3D space.
+
+    Methods:
+        __init__(self, parent=None): Initializes the MoveObjectDialog.
+        get_values(self): Returns the entered displacement values as floats.
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Move object")
+
+        self.dx_spin_box = FormItem("dx", width = 30, parent=self)
+        self.dy_spin_box = FormItem("dy", width = 30, parent=self)
+        self.dz_spin_box = FormItem("dz", width = 30, parent=self)
+        self.spin_box_layout = QVBoxLayout()
+        self.spin_box_layout.setSpacing(1)
+        self.spin_box_layout.addWidget(self.dx_spin_box)
+        self.spin_box_layout.addWidget(self.dy_spin_box)
+        self.spin_box_layout.addWidget(self.dz_spin_box)
+
+        self.ok_button = QPushButton("OK", self)
+        self.ok_button.clicked.connect(self.accept)
+
+        self.cancel_button = QPushButton("Cancel", self)
+        self.cancel_button.clicked.connect(self.reject)
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.ok_button)
+        button_layout.addWidget(self.cancel_button)
+
+        main_layout = QVBoxLayout(self)
+        main_layout.addLayout(self.spin_box_layout)
+        main_layout.addLayout(button_layout)
+
+        self.setLayout(main_layout)
+
+    def get_values(self):
+        """
+        Returns the entered displacement values as floats.
+
+        Returns:
+            Tuple[float, float, float]: The displacement values entered in the dialog.
+        """
+        return float(self.dx_spin_box.value()), float(self.dy_spin_box.value()), float(self.dz_spin_box.value())
+
+class FormItem(QWidget):
+    def __init__(self, name, unit=None, width=50, min=-float('inf'), max=float('inf'), decimals=2, parent=None):
+        """
+        Initialize the RayTracing object.
+
+        Parameters:
+        - name (str): The name of the object.
+        - unit (str, optional): The unit of measurement for the object. Defaults to None.
+        - width (int, optional): The width of the label. Defaults to 50.
+        - min (float, optional): The minimum value for the spin box. Defaults to -inf.
+        - max (float, optional): The maximum value for the spin box. Defaults to inf.
+        - decimals (int, optional): The number of decimal places to display. Defaults to 2.
+        - parent (QWidget, optional): The parent widget. Defaults to None.
+        """
+        super().__init__(parent)
+
+        self.name = name
+        self.spin_box = QDoubleSpinBox(self)
+        self.spin_box.setDecimals(decimals)
+        self.spin_box.setMinimum(min)
+        self.spin_box.setMaximum(max)
+        self.spin_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+
+        label_text = f"{name} {unit}" if unit else f"{name}"
+        label = QLabel(label_text, self)
+        label.setFixedWidth(width)
+        label_colon = QLabel(":", self)
+        label_colon.setFixedWidth(10)
+
+        form_layout = QGridLayout()
+        form_layout.setSpacing(0)
+        form_layout.setContentsMargins(0, 0, 0, 0)
+        form_layout.addWidget(label, 0, 0)
+        form_layout.addWidget(label_colon, 0, 1)
+        form_layout.addWidget(self.spin_box, 0, 2)
+
+        self.setLayout(form_layout)
+
+    def value(self):
+        """
+        Returns the value of the spin box.
+        """
+        return self.spin_box.value()
 
 def main():
     """
