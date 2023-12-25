d42f88577a37307929f34a49951ad0f5f01e34dd
Bryan Casanelli <bcasanelli@dparadig.com>
2023-12-25 13:03:46 +0000

    Improved the support for objects without faces, and to load rectangles.

--- source/Polyhedron.py
+++ source/Polyhedron.py
@@ -10,7 +10,7 @@ class Polyhedron:
     Represents a 3D object composed of TriangularPlanarPolygons and/or RectangularPlanarPolygons.
 
     Attributes:
-        faces (list of TriangularPlanarPolygon or RectangularPlanarPolygon): The faces of the 3D object.
+        faces (list of TriangularPlanarPolygon): The faces of the 3D object.
         material (Material): The material of the Polyhedron. If no material path is provided,
                         a vacuum material (refractive index of 1) is created by default.
         vertices (list of Point): The vertices of the Polyhedron.
@@ -58,6 +58,7 @@ class Polyhedron:
             filename (str): The path to the OBJ file.
         """
         current_line = 0
+        tmp_vertex = []
         with open(filename, 'r') as file:
             if self.progress_callback_function is not None:
                 total_lines = sum(1 for _ in file)
@@ -69,26 +70,19 @@ class Polyhedron:
                     self.progress_callback_function(progress)
                 if line.startswith('v '):
                     parts = line.split()
-                    point = Point(float(parts[1]), float(parts[2]), float(parts[3]))
-                    self.vertices.append(point)
+                    tmp_vertex.append(Point(float(parts[1]), float(parts[2]), float(parts[3])))
                 elif line.startswith('f '):
                     parts = line.split()
                     indices = [int(part.split('/')[0]) - 1 for part in parts[1:]]  # OBJ indices start at 1
                     if len(indices) == 3:
                         # It's a triangle
-                        triangle = TriangularPlanarPolygon([self.vertices[i].copy() for i in indices])
-                        self.face_indices.append(indices)
-                        self.add_face(triangle)
+                        triangle = TriangularPlanarPolygon([tmp_vertex[i].copy() for i in indices])    
+                        self.add_face(triangle, False)
                     elif len(indices) == 4:
                         # It's a rectangle, create a RectangularPlanarPolygon
-                        rectangle = RectangularPlanarPolygon([self.vertices[i].copy() for i in indices])
-                        # Decompose the rectangle into two triangles
-                        self.add_face(rectangle.triangle1)
-                        self.face_indices.append([indices[0], indices[1], indices[2]])
-                        self.add_face(rectangle.triangle2)
-                        self.face_indices.append([indices[2], indices[3], indices[0]])
-
-        
+                        rectangle = RectangularPlanarPolygon([tmp_vertex[i].copy() for i in indices])
+                        self.add_face(rectangle, False)
+        self.clean_vertices()
 
     def _are_points_distinct(self, points):
         """
@@ -102,28 +96,59 @@ class Polyhedron:
         """
         return len(points) == len(set((p.x, p.y, p.z) for p in points))
 
-    def add_face(self, polygon):
+    def add_face(self, polygon, clean_vertices = True):
         """
-        Adds a face to the 3D object only if all points are distinct. 
-        Emits a warning otherwise.
+        Adds a face to the 3D object.
 
         Args:
             polygon (TriangularPlanarPolygon or RectangularPlanarPolygon): The face to add to the 3D object.
         """
-        if isinstance(polygon, TriangularPlanarPolygon):
-            if self._are_points_distinct(polygon.vertices):
-                self.faces.append(polygon)
-            else:
-                warnings.warn("Attempted to add a triangular face with non-distinct points.", RuntimeWarning)
-        elif isinstance(polygon, RectangularPlanarPolygon):
-            if self._are_points_distinct(polygon.points):
-                # Decompose the rectangle into two triangles and add them
-                self.faces.append(polygon.triangle1)
-                self.faces.append(polygon.triangle2)
-            else:
-                warnings.warn("Attempted to add a rectangular face with non-distinct points.", RuntimeWarning)
-        else:
+        if not isinstance(polygon, (TriangularPlanarPolygon, RectangularPlanarPolygon)):
             raise TypeError("Unsupported polygon type.")
+        
+        if isinstance(polygon, RectangularPlanarPolygon):
+            self.add_face(polygon.triangle1)
+            self.add_face(polygon.triangle2)
+        else:
+            # Add vertices from the polygon to the polyhedron's vertices list
+            face_vertex_indices = []
+            for vertex in polygon.vertices:
+                self.vertices.append(vertex.copy())
+                face_vertex_indices.append(len(self.vertices) - 1)  # Index of the newly added vertex
+
+            # Add the new face's vertex indices to face_indices
+            self.face_indices.append(face_vertex_indices)
+
+            # Add the face to the faces list
+            self.faces.append(polygon)
+
+        # Clean the vertices if requested
+        if clean_vertices:
+            self.clean_vertices()
+
+    def clean_vertices(self):
+        """
+        Removes duplicate vertices and updates the face indices.
+        """
+        unique_vertices = {}
+        new_vertices = []
+        new_face_indices = []
+
+        # Identify unique vertices and assign them new indices
+        for vertex in self.vertices:
+            vertex_tuple = (vertex.x, vertex.y, vertex.z)
+            if vertex_tuple not in unique_vertices:
+                unique_vertices[vertex_tuple] = len(new_vertices)
+                new_vertices.append(vertex)
+
+        # Update face indices based on new vertex indices
+        for face in self.face_indices:
+            new_face = [unique_vertices[(self.vertices[i].x, self.vertices[i].y, self.vertices[i].z)] for i in face]
+            new_face_indices.append(new_face)
+
+        # Update the vertices and face_indices of the Polyhedron
+        self.vertices = new_vertices
+        self.face_indices = new_face_indices
         
     def translate(self, dx, dy, dz):
         """
@@ -134,20 +159,28 @@ class Polyhedron:
             dy (float): The amount to translate in the y direction.
             dz (float): The amount to translate in the z direction.
         """
-        for i, vertex in enumerate(self.vertices):
-            vertex.x += dx
-            vertex.y += dy
-            vertex.z += dz
-            if self.progress_callback_function != None:
-                self.progress_callback_function((i+1) / len(self.vertices) * 100)
-
-        for i, face in enumerate(self.faces):
-            for vertex in face.vertices:
+        if self.vertices:
+            # Translate all vertices
+            for i, vertex in enumerate(self.vertices):
                 vertex.x += dx
                 vertex.y += dy
                 vertex.z += dz
-            if self.progress_callback_function != None:
-                self.progress_callback_function((i+1) / len(self.faces) * 100)
+                if self.progress_callback_function != None:
+                    self.progress_callback_function((i+1) / len(self.vertices) * 100)
+
+            # Translate all vertices of each face
+            for i, face in enumerate(self.faces):
+                for vertex in face.vertices:
+                    vertex.x += dx
+                    vertex.y += dy
+                    vertex.z += dz
+                if self.progress_callback_function != None:
+                    self.progress_callback_function((i+1) / len(self.faces) * 100)
+
+        # Translate the reference point
+        self.reference.x += dx
+        self.reference.y += dy
+        self.reference.z += dz
 
     def change_reference_point(self, ref_type, axis, x=None, y=None, z=None):
         """
@@ -161,13 +194,12 @@ class Polyhedron:
             y (float): The y-coordinate of the new reference point (optional).
             z (float): The z-coordinate of the new reference point (optional).
         """
-        if not self.vertices:
-            return
-
         if ref_type == "Lowest":
-            self.reference = min(self.vertices, key=lambda vertex: getattr(vertex, axis))
+            if self.vertices:
+                self.reference = min(self.vertices, key=lambda vertex: getattr(vertex, axis))
         elif ref_type == "Highest":
-            self.reference = max(self.vertices, key=lambda vertex: getattr(vertex, axis))
+            if self.vertices:
+                self.reference = max(self.vertices, key=lambda vertex: getattr(vertex, axis))
         elif ref_type == "Manual":
             if x is None or y is None or z is None:
                 raise ValueError("Invalid coordinates for manual reference point.")
